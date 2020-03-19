263f2d8eae22c70f4145849be79ee7d0d4ca79a7
Mickdevv <46746774+Mickdevv@users.noreply.github.com>
2020-03-19 16:07:24 +0000

    Update MichaelCarScript.ino

--- Backend/Code/ControllerCode/MichaelCarScript/MichaelCarScript.ino
+++ Backend/Code/ControllerCode/MichaelCarScript/MichaelCarScript.ino
@@ -1,8 +1,8 @@
 const int Y_pin = 0; // analog pin connected to X output
 const int X_pin = 1; // analog pin connected to Y output
 
-int enableLPin = 3; // pin 1 on L293D IC
-int enableRPin = 9; // pin 9 on L293D IC
+int enableLPin = 9; // pin 1 on L293D IC
+int enableRPin = 3; // pin 9 on L293D IC
 
 int motorLPinF = 4; // pin 2 on L293D IC
 int motorLPinB = 5; // pin 7 on L293D IC
@@ -46,12 +46,34 @@ void loop() {
     //Serial.print(state);
     flag = 0;
   }
-  xReading = analogRead(X_pin)/2 - 256;
-  yReading = -(analogRead(Y_pin)/2 - 256);
+  xReading = analogRead(X_pin);
+  xReading = map(xReading, 0, 1023, -255, 255);
+  
+  yReading = analogRead(Y_pin);
+  yReading = map(yReading, 0, 1023, -255, 255);
   
   vRaw = sqrt(pow(yReading,2) + pow(xReading,2));
-  v = 256*vRaw/363;
+  //v = 256*vRaw/363;
+  v=map(vRaw, 0, 363, 0, 255);
 
+//  if (yReading>=0)
+//  {
+//    if (xReading>10)
+//    {
+//      motorL = v;
+//      motorR = v-(2*xReading);
+//    }
+//    else if (xReading<-10)
+//    {
+//      motorL = v+(2*xReading);
+//      motorR = v;
+//    }
+//  }
+//else 
+//    {
+//      motorL = v;
+//      motorR = v;
+//    }
   if (xReading >= 0 && yReading >= 0)
       motorL = v;
       motorR = v - 2*xReading;
@@ -70,8 +92,8 @@ void loop() {
 
    
 
-//  motorL = yReading;
-//  motorR = yReading;
+  motorL = -motorL;
+  motorR = -motorR;
 
   Serial.print("JOYSTICK  ");
   Serial.print(xReading);
@@ -87,7 +109,7 @@ void loop() {
   Serial.print(v);
   
   Serial.println("");
-  if (abs(motorL) > 10 or abs(motorR) > 10)
+  if (abs(xReading) > 10 or abs(yReading) > 10)
   {
       if (motorL >= 0)
         {
