6326a7f1e0d2964da830d63a8b246193f5ddb120
MickDevv <michael@devereux.fm>
2020-04-15 17:57:54 +0200

    CarScript comments

--- Backend/Code/ControllerCode/MichaelCarScript/MichaelCarScript.ino
+++ Backend/Code/ControllerCode/MichaelCarScript/MichaelCarScript.ino
@@ -1,5 +1,4 @@
-//const int Y_pin = 0; // analog pin connected to X output
-//const int X_pin = 1; // analog pin connected to Y output
+
 
 int enableLPin = 3; // pin 1 on L293D IC
 int enableRPin = 9; // pin 9 on L293D IC
@@ -15,22 +14,26 @@ int motorR = 0;
 
 int xReading = 0;
 int yReading = 0;
+
+//Default values in case of communication failure so the car remins stationary
 int xReadingBT = 499;
 int yReadingBT = 499;
 
+//Character used to temporarily store value sent over bluetooth
 char c;
+//Used to store entire instructions for the car
 String instruction = "";
-
-float v;
-float vRaw;
+//Temporarily store parts of the instructions of the car
 String readData;
+
+//Magnitude of velocity the car should have 
+float v;
+
 int flag = 0;      //makes sure that the serial only prints once the state
 int stateStop = 0;
-float normalize = 256/363;
 int counter = 0;
 
 void setup() {
-  // sets the pins as outputs:
 
   //in the motor pin naming scheme, L is for left, R for right, F for forwards and B for backwards
   pinMode(motorLPinF, OUTPUT);
@@ -52,38 +55,42 @@ void loop() {
   
   while (Serial.available()>0)
   {
-    counter++;
-    //Serial.println("Hey, you're receiving cereal!!! ");
+    //Reading the character sent by the controller
     c = Serial.read();
+    //building the instruction as it receives characters
     instruction+=c;
-    //Serial.print(String(c));  
+    //Determines whether the character is a separator. if it isn't then it must be part of a reading
     if (c!=',' && c!='E')
     {
       readData += c;
     }
+    //Used to separate readings. Once it encounters this character it knows that the first reading is received
     else if (c==',')
     {
+      //if statement used to ignore any values that don't fall within the range of 0-999
       if (readData.toInt() <= 999 and readData.toInt() >= 0)
       {
         xReadingBT = readData.toInt();
       }
+      //wipes readData so that it is ready to receive the second reading
       readData = "";
     }
+    //the E character is used to denote the end of an instruction. This means the secod reading is received and useable
     else if (c=='E')
     {
-      //Serial.print('2');
       if (readData.toInt() <= 999 and readData.toInt() >= 0)
-      
       {
         yReadingBT = readData.toInt();
       }
       readData="";
-      Serial.print(instruction);
+      //Used for 
       for (int i = 0;  i+instruction.length() < 9; i++)
       {
         Serial.print(" ");
       }
+      //alerts the program that an instruction was successfully received
       flag = 1;
+      //wipes instruction, ready for the next one 
       instruction = "";
       break;
     }
@@ -95,7 +102,6 @@ void loop() {
     {
       Serial.print("         ");
     }
-  //Serial.flush();
 
 
 
@@ -112,10 +118,7 @@ void loop() {
   }
  
   
-  //the maximum value for vRaw is 363 which needs reducing to 255
-  //v = map(vRaw, 0, 363, 0, 255);
-
-  //Different steering quadrants
+  //Different steering quadrants for the joystick
   if (xReading >= 0 && yReading >= 0)
   {
       motorL = v;
@@ -140,18 +143,14 @@ void loop() {
       motorR = -v -xReading;
   }
   
-   
 
-//  motorL = yReading;
-//  motorR = yReading;
-
-  
-
-  //the joystick rests slightly off of (0,0), so this means that if it isn't more than 10 away from the centre then the robot should stay at rest
+  //the joystick rests slightly off of (0,0), so this means that if it isn't more
+  //than 10 away from the centre then the robot should stay at rest
   
   if (v>20)
   {
-    //if stateents to determine which pins to set high depending on the speed set to each motor
+    //if statements to determine which pins to set 
+    //high depending on the speed set to each motor
       if (motorL >= 0)
         {
           digitalWrite(motorLPinF, HIGH);
@@ -174,7 +173,8 @@ void loop() {
           digitalWrite(motorRPinF, LOW);
           digitalWrite(motorRPinB, HIGH);
         }
-        
+
+        //used to cap the motor speed and direction between the ranges of -255 and 255
         if (abs(motorL)>=255)
         {
           motorL = 255* motorL / abs(motorL);
@@ -183,9 +183,6 @@ void loop() {
         {
           motorR = 255* motorR / abs(motorR); 
         }
-    //if you open the serial monitor, you can see the values of the joystick readings, the speeds written to each motor and the value of v
-  
-    
         analogWrite(enableLPin, abs(motorL));
         analogWrite(enableRPin, abs(motorR));
   }
@@ -198,7 +195,7 @@ void loop() {
       digitalWrite(motorRPinB, LOW);
   }
 
-  
+  //Used to monitor what the controller is sending vs what the car is receiving and doing
   Serial.print("JOYSTICK  ");
   Serial.print(xReading);
   Serial.print(", ");
